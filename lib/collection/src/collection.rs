6ecfa0741795bcb819ed5ac2acaff16349c6f34f
Roman Titov <ffuugoo@users.noreply.github.com>
2023-09-07 19:35:21 +0200

    Check that segment is compatible with collection on collection load

--- lib/collection/src/collection.rs
+++ lib/collection/src/collection.rs
@@ -1849,6 +1849,10 @@ impl Collection {
             .get_shard(&shard_id)
             .ok_or_else(|| shard_not_found_error(shard_id))?;
 
+        // TODO:
+        //   Check that shard snapshot is compatible with the collection
+        //   (see `VectorsConfig::check_compatible_with_segment_config`)
+
         replica_set
             .restore_local_replica_from(snapshot_shard_path)
             .await
